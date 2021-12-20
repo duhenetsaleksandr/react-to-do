b03b104e88fbb77d32bcb544fd7f404bf352680e
duhenetsaleksandr <duhenets@gmail.com>
2021-12-21 00:08:57 +0200

    create helpers file

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Loader from './components/Loader';
 import * as constants from './constants/constants';
-
-const fetchTodo = async (amountTodos = 5) => {
-    const response = await axios.get(`${constants.API_URL}?_limit=${amountTodos}`);
-    return response.data;
-}
+import fetchTodo from './helpers';
 
 export default function App () {
     const [ loader, setLoader ] = useState(false);
